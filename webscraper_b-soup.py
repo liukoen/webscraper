194f03bf9f0eae089b6861b34ba5077159c8158c
Koen Liu <koen.liu@gmail.com>
2018-07-02 19:46:55 +0200

    webscraper with bs4

--- webscraper_b-soup.py
+++ webscraper_b-soup.py
@@ -1,12 +1,12 @@
 import bs4
 import requests
 import re
-
+regex_ex = "((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
 
 req = requests.get("http://www.sheldonbrown.com/web_sample1.html")
 html_code=req.text
 soup = bs4.BeautifulSoup(html_code, "html.parser")
-
+url=[]
 #print(soup.prettify())
 
 links=soup.find_all(href=True)
@@ -16,11 +16,11 @@ links=soup.find_all(href=True)
 for link in links:
     link_str=str(link)
     #print(link_str)
-    if re.match("(www|http:|https:)+[^\s]+[\w]",link_str):
-        print(link_str)
-
+    if re.search(regex_ex,link_str):
+        url.append(re.search(regex_ex,link_str).group(0))
+     #   print(ma)
     #print(ur)
-
+print(url)
 
 
 
